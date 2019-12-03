0e8138afc4fcb01dd1d13f2cb243c54ac03359d9
codingphun <41023659+codingphun@users.noreply.github.com>
2019-12-03 14:29:14 -0800

    removed qemu and docker dependencies

--- assets/scripts/install_deps.sh
+++ assets/scripts/install_deps.sh
@@ -112,22 +112,6 @@ cp -a deps/* /etc/ros/rosdep/sources.list.d/
 echo "yaml file:///$WORK_DIR/jetbot.yaml" > /etc/ros/rosdep/sources.list.d/21-customdepenencies.list
 sudo -u ubuntu rosdep update
 
-# The following command logs in to the an AWS Elastic Container Repository (ECR) to
-# enable your machine to pull a base docker image
-
-$(aws ecr get-login --no-include-email --registry-ids 593875212637 --region us-east-1)
-
-# Install Ubuntu dependencies for cross compilation:
-apt-get update
-apt-get install -y qemu-user-static
-if [ $? -ne 0 ]; then
-    echo "Failed to install QEMU. Please wait for a few minutese and re-run this script."
-    exit 1
-fi
-
-#Build Docker Container
-echo "Building docker image for robot ..."
-docker build -t jetbot-ros -f Dockerfile .
 
 #fix ros permissions
 rosdep fix-permissions
